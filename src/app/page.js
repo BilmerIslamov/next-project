6dec3567fd738a9ca95b6e5b0a0f1c62ae789853
BilmerIslamov <bilmerlibrary@gmail.com>
2023-08-24 13:10:49 +0500

    news

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,27 @@
 async function getData() {
-    const res = await fetch('https://bilmervipapi.pythonanywhere.com/next_api/all-news/')
+    const res = await fetch('https://bilmervipapi.pythonanywhere.com/next_api/all-news/', { next: { revalidate: 3 } })
     if (!res.ok) {
         throw new Error('Failed to fetch data')
     }
 
     return res.json()
+    
 }
 
-export default async function Page() {
+export default async function Home() {
     const data = await getData()
     console.log(data);
+    
+    
+    
 
-    return <main></main>
+    return <main>
+        {data.map((item) => {
+            return (
+                <>
+                <h2>{item.news_title}</h2>
+            </>
+            )
+        })}
+    </main>
 }
